ee10b5711c816811804ed8d7376a3d6578083f48
BOTZEMPIRE <chaudharyji0805@gmail.com>
2026-01-12 15:32:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 API_ID = 31573732
 API_HASH = "ecebdea86eae3370dd8138bc0b9a385b"
-BOT_TOKEN = "8256841104:AAEt5iMXBxa7pcx_PySDiNX39NHE7zreaK8"
-OWNER_ID = 8583334266
-LOG_CHANNEL = "@CutiesMusicBot" # Replace with your channel ID or "@channelusername"
+BOT_TOKEN = "5453914355:AAEv4RA7Y_BtXQO-re4xIu-JU0KN10qirKM"
+OWNER_ID = 7538572906
+LOG_CHANNEL = "@BotColony" # Replace with your channel ID or "@channelusername"
+
